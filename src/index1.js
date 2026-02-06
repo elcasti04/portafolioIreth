ce028a16cef8c20c917fc1abffa311a8d8a0f0df
AndresCastro <188876553+elcasti04@users.noreply.github.com>
2026-02-06 01:29:24 -0500

    Agregando nuevas secciones

---
+++ src/index1.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+	/* -------------------- DESCARGAR CV -------------------- */
+	const btnCV = document.getElementById('Descargar-Cv');
+	if (btnCV) {
+		btnCV.addEventListener('click', function () {
+			const cvUrl = 'documents/curriculum.pdf';
+			const link = document.createElement('a');
+			link.href = cvUrl;
+			link.download = 'Curriculum CV Ireth Franco.pdf';
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		});
+	}
+
+	/* -------------------------cambiar Idioma----------------------------*/
+	const idiomabtn = document.getElementById('Idioma');
+	if (idiomabtn) {
+		idiomabtn.addEventListener('click', function (e) {
+			e.preventDefault();
+			const file = window.location.pathname.split('/').pop() || 'index.html';
+			const newUrl =
+				file === 'index.html' || file === '' ? 'index-en.html' : 'index.html';
+			window.location.href = newUrl;
+		});
+	}
+
+	/* -------------------------cambiar Modo----------------------------*/
+	const modobtn = document.getElementById('modo');
+	if (modobtn) {
+		modobtn.addEventListener('click', function () {
+			document.body.classList.toggle('light');
+		});
+	}
+});
